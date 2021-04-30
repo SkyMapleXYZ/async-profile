030652ec0ce16a97da64d45c1f5815eb78e8a172
GitHub Action <action@github.com>
2021-04-30 03:30:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/yfc/asyncprofile/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yfc/asyncprofile/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yfc.asyncprofile.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yfc.asyncprofile.MyBundle
 
 class MyApplicationService {
 
